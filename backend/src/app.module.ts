6a8c5d30afeafc5517827d977569f7ad408719c9
Veysel Emir Yurtseven <veyselemiryurtseven@Veysel-MacBook-Air.local>
2025-09-09 18:16:53 +0300

    User Module Finished

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,40 +1,19 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { APP_FILTER, APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommonModule } from './common/common.module';
 import { PresentationModule } from './presentation/presentation.module';
 import { ApplicationModule } from './application/application.module';
 import { InfrastructureModule } from './infrastructure/infrastructure.module';
-import { HttpExceptionFilter, AllExceptionsFilter } from './common/filters/http-exception.filter';
-import { ValidationPipe } from './common/pipes/validation.pipe';
-import appConfig from './config/app.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [appConfig],
-    }),
+    CommonModule,
     InfrastructureModule,
     ApplicationModule,
     PresentationModule,
   ],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_FILTER,
-      useClass: AllExceptionsFilter,
-    },
-    {
-      provide: APP_FILTER,
-      useClass: HttpExceptionFilter,
-    },
-    {
-      provide: APP_PIPE,
-      useClass: ValidationPipe,
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
--- backend/src/presentation/presentation.module.ts
+++ backend/src/presentation/presentation.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './controllers/user.controller';
 import { TaskController } from './controllers/task.controller';
 import { ApplicationModule } from '../application/application.module';
 
 @Module({
   imports: [ApplicationModule],
-  controllers: [UserController, TaskController],
+  controllers: [TaskController],
 })
 export class PresentationModule {}
